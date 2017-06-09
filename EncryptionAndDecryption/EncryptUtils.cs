6522569d8c2822e98ac978a732901bc581e2923f
syx <syxtest01@gmail.com>
2017-06-09 11:34:39 +0800

    Create EncryptUtils.cs

--- EncryptionAndDecryption/EncryptUtils.cs
+++ EncryptionAndDecryption/EncryptUtils.cs
@@ -246,7 +246,65 @@ namespace EncryptionAndDecryptionLibary
                 }
             }
         }
-
+        
+       /// <summary>
+       /// DES加密算法(适配java)
+       /// </summary>
+       /// <param name="encryptString">要加密的字符串</param>
+       /// <param name="sKey">加密码Key</param>
+       /// <returns>正确返回加密后的结果，错误返回源字符串</returns>
+       public static string ToDESEncrypt(string encryptString, string sKey)
+       {
+           try
+           {
+ 
+               byte[] keyBytes = Encoding.UTF8.GetBytes(sKey);
+               byte[] keyIV = keyBytes;
+               byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+ 
+               DESCryptoServiceProvider desProvider = new DESCryptoServiceProvider();
+ 
+               // java 默认的是ECB模式，PKCS5padding；c#默认的CBC模式，PKCS7padding 所以这里我们默认使用ECB方式
+               desProvider.Mode = CipherMode.ECB;
+               MemoryStream memStream = new MemoryStream();
+               CryptoStream crypStream = new CryptoStream(memStream, desProvider.CreateEncryptor(keyBytes, keyIV), CryptoStreamMode.Write);
+ 
+               crypStream.Write(inputByteArray, 0, inputByteArray.Length);
+               crypStream.FlushFinalBlock();
+               return Convert.ToBase64String(memStream.ToArray());
+ 
+           }
+           catch(Exception ex)
+           {
+               return encryptString;
+           }
+       }
+ 
+ 
+       /// <summary>
+       /// DES解密算法(适配java)
+       /// </summary>
+       /// <param name="decryptString">要解密的字符串</param>
+       /// <param name="sKey">加密Key</param>
+       /// <returns>正确返回加密后的结果，错误返回源字符串</returns>
+       public static string ToDESDecrypt(string decryptString, string sKey)
+       {
+           byte[] keyBytes = Encoding.UTF8.GetBytes(sKey);
+           byte[] keyIV = keyBytes;
+           byte[] inputByteArray = Convert.FromBase64String(decryptString);
+ 
+           DESCryptoServiceProvider desProvider = new DESCryptoServiceProvider();
+ 
+           // java 默认的是ECB模式，PKCS5padding；c#默认的CBC模式，PKCS7padding 所以这里我们默认使用ECB方式
+           desProvider.Mode = CipherMode.ECB;
+           MemoryStream memStream = new MemoryStream();
+           CryptoStream crypStream = new CryptoStream(memStream, desProvider.CreateDecryptor(keyBytes, keyIV), CryptoStreamMode.Write);
+ 
+           crypStream.Write(inputByteArray, 0, inputByteArray.Length);
+           crypStream.FlushFinalBlock();
+           return Encoding.Default.GetString(memStream.ToArray());
+ 
+       }
         #endregion DES加密解密
 
         #region 3DES 加密解密   key要求必须为字符串长度为24字节Base64编码之后的byte数组
